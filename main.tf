777204778df4a3038793f06bc0aebf89f19e13d6
Kreyno93 <michael.wagener@neuefische.de>
2024-12-09 14:38:30 +0100

    adding TPL and SH script to see the difference

--- main.tf
+++ main.tf
@@ -7,14 +7,21 @@ resource "aws_instance" "example" {
   instance_type = var.instance_type
   key_name      = "vockey"
 
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Instance launched successfully!" > /tmp/message.txt
-    echo "Variable 1: ${var.variable_1}" >> /tmp/message.txt
-    echo "Variable 2: ${var.variable_2}" >> /tmp/message.txt
-  EOF
+  user_data = file("userdata.sh")
 
   tags = {
-    Name = "Simple-EC2"
+    Name = "Simple-EC2-SH"
+  }
+}
+
+resource "aws_instance" "example2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "vockey"
+
+  user_data = file("userdata.tpl")
+
+  tags = {
+    Name = "Simple-EC2-TPL"
   }
 }
